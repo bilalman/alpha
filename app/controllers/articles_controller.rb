0407cb64152674237e7c73e84862128c878b9242
bilalman <bg962618@gmail.com>
2024-04-04 15:53:49 +0500

    CRUD Operation for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,44 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find (params[:id])
   end
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find (params[:id])
+
+  end
+
+  def create
+    # render plain: params[:article]
+    @article =Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article.inspect
+    if @article.save
+      flash[:notice] = "Article was Created Successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article Was Updated Successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   validates:title, presence:true, length:{minimum:7, maximum:100}
-  validates:description, presence:true, length:{minimum:50, maximum:400}
+  validates:description, presence:true, length:{minimum:10, maximum:400}
 
 end
