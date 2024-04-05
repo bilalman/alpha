86547250cd518ad177cbb32dfcf449f55a38ec77
bilalman <bg962618@gmail.com>
2024-04-05 06:24:30 +0500

    DRY Method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
-    @article = Article.find (params[:id])
   end
   def index
     @articles = Article.all
@@ -11,7 +11,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find (params[:id])
 
   end
 
@@ -28,7 +27,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(params.require(:article).permit(:title, :description))
       flash[:notice] = "Article Was Updated Successfully"
       redirect_to @article
@@ -38,8 +36,12 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
