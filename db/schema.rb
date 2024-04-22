bb7611df5c86dc928d64471bd242e7d250bcf2e2
bilalman <bg962618@gmail.com>
2024-04-22 13:05:24 +0500

    password assigned to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_19_122048) do
+ActiveRecord::Schema.define(version: 2024_04_22_075403) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2024_04_19_122048) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
